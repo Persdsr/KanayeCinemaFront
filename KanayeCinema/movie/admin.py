eed9bbc6b708e658490a3e85176b055f927f38fb
Persdsr <reixter@bk.ru>
2023-08-24 12:48:00 +0500

    24.08.2023

--- KanayeCinema/movie/admin.py
+++ KanayeCinema/movie/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Movie, Category, Genre, Actor, Career, ActorImages
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ActorImages)
+class ActorImagesAdmin(admin.ModelAdmin):
+    pass
